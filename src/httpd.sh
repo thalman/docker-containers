15e25c23324b64b78839cf621e4a8679e3adbfb7
Tomas Halman <thalman@redhat.com>
2019-04-08 09:30:34 +0200

    Problem: httpd shutdown kill itselfs
    Solution: Filter script from process list,
    wait tor apache/httpd to shutdown, return
    propper exit status

--- src/httpd.sh
+++ src/httpd.sh
@@ -1,23 +1,31 @@
-#!/bin/sh
+#!/bin/sh -x
 
 PIDFILE=/var/run/apache.pid
+RET=0
 
 start() {
+    RET=0
     nohup /usr/sbin/httpd -DFOREGROUND >/dev/null 2>&1 </dev/null &
     PID=$!
     echo $PID >$PIDFILE
     sleep 1
-    kill -0 $PID >/dev/null 2>&1 || exit 1
+    kill -0 $PID >/dev/null 2>&1 || RET=1
 }
 
 stop() {
+    RET=0
     if [ -f $PIDFILE ] ; then
-        kill -WINCH `cat $PIDFILE`
-        sleep 1
+        PIDS=`cat $PIDFILE`
+        kill -TERM $PIDS
+        for a in 1 2 3 4 5; do
+            sleep 1
+            kill -0 $PIDS >/dev/null 2>&1 || break;
+        done
         rm -f $PIDFILE
     fi
-    PIDS=`ps -ef  | grep /usr/sbin/http[d] | awk '{ print $2 }'`
+    PIDS=`ps -ef  | grep /usr/sbin/httpd | grep -v grep | awk '{ print $2 }'`
     if [ "$PIDS" != "" ]; then
+        RET=1
         kill -9 $PIDS
     fi
 }
@@ -34,3 +42,6 @@ case "$1" in
         start
         ;;
 esac
+
+exit $RET
+
